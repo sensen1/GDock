c851121592de6c2816b3de0f12d28d50458e09d7
kyarazhan <shangrilas@live.com>
2020-06-14 00:30:21 +0800

    初始化自定义设置

---
+++ lede_diy-part2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+
+# Modify default IP
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+# Modify default host name
+sed -i 's/Openwrt/Kyara/g' package/base-files/files/bin/config_generate
+# Modify default wireless name
+sed -i 's/Openwrt/Kyara/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#Clone SSRPlus source code
+git clone https://github.com/fw876/helloworld package/SSRPlus
