5591e2e420f13e343793ce42e5954af368988746
kyarazhan <shangrilas@live.com>
2020-09-12 00:09:45 +0800

    更改主题至2.X版本

--- lede_d_diy-part2.sh
+++ lede_d_diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 # 删除默认argon主题，并下载新argon主题
 rm -rf ./package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon/
+git clone https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon/
 #git lua-maxminddb 依赖
 git clone https://github.com/jerrykuku/lua-maxminddb.git ./package/lean/
 # Modify default IP
